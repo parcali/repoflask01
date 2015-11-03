507c8b16a4e9d265e9bed0561876c9aa904b09c6
ahmet gunel <ahmetgunel@gmail.com>
2015-11-03 12:01:08 +0300

    routelar eklendi

--- app.py
+++ app.py
@@ -14,6 +14,35 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+# dynamic route 
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value +1 
+	return "correct value+1"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value +1 
+	return "correct float value+1"
+
+#dynamic route that accepts slashes 
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correct path"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "ahmet" :
+    	return "Hello, {}".format(name), 200
+    else:
+    	return "Not Found", 400
+
+
 # start the development server using the run() method
 if __name__ == "__main__":
     app.run()
